6113f34de8ed203d1b53f9bd234df95fb72adb1f
Hiroyuki Itabashi <itabashi@atma.co.jp>
2023-05-18 16:27:10 +0900

    temp

--- app.js
+++ app.js
@@ -1,68 +1,84 @@
-const NEWS_API_KEY = "67ece92e536e4852a075c3a35621b9ea"; // 自分のapiキー
-const main = document.querySelector("main");
-const sourceSelector = document.querySelector("#sourceSelector");
-const defaultSource = "abc-news-au";
+const NEWS_API_KEY = '67ece92e536e4852a075c3a35621b9ea'; // 自分のapiキー
+const main = document.querySelector('main');
+const sourceSelector = document.querySelector('#sourceSelector');
+const defaultSource = 'abc-news-au';
 
-const publicVapidKey = "BLGgW2eVUdKoSx2R4k80hCsTSLKPd0YmvHHm2CaW5JfXIlHm92sMHMUGOgBHpaweTRERkCyrT_42cDTmtWCF6zo";
+const publicVapidKey =
+  'BNWDl9RZjaY2lCkWfRflg_MsM4BG0-UEVbLyQ66QQ_U0JZwBmmpOVfvRtkWoQxNQ8aqnIuQxtJ6RkFVPX5v1fwU';
 
 (async () => {
-    updateNews();
-    await updateSources();
+  // updateNews();
+  // await updateSources();
 
-    // 画面表示時のプルダウンの初期値を設定
-    sourceSelector.value = defaultSource;
-    sourceSelector.addEventListener("change", e => {
-        updateNews(e.target.value);
-    });
+  // 画面表示時のプルダウンの初期値を設定
+  sourceSelector.value = defaultSource;
+  sourceSelector.addEventListener('change', (e) => {
+    updateNews(e.target.value);
+  });
 
-    if ("serviceWorker" in navigator) {
-        try {
-            register = await navigator.serviceWorker.register("sw.js")
-            console.log("SW registered")
+  if ('serviceWorker' in navigator) {
+    try {
+      register = await navigator.serviceWorker.register('sw.js');
+      console.log('SW registered');
 
-            const subscription = await register.pushManager.subscribe({
-                userVisibleOnly: true,
-                applicationServerKey: urlBase64ToUint8Array(publicVapidKey),
-            });
-            console.log("Push registered");
+      const subscription = await register.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array(publicVapidKey),
+      });
+      console.log('createelement start')
+      const div = document.createElement('div');
+      console.log('createelement end')
+      console.log('div start')
+      div.innerHTML = JSON.stringify(subscription);
+      console.log('div end')
+      console.log('document start')
+      document.body.appendChild(div);
+      console.log('document end')
+      console.log('Push registered');
+      console.log(subscription);
 
-            fetch("http://localhost:8081", {
-                method: "POST",
-                body: JSON.stringify(subscription),
-                headers: {
-                    "Content-Type": "application/json",
-                },
-            });
-            console.log("Push Sent");
-        } catch (e) {
-            console.log("SW faild")
-        }
+      // fetch("http://localhost:8081", {
+      //     method: "POST",
+      //     body: JSON.stringify(subscription),
+      //     headers: {
+      //         "Content-Type": "application/json",
+      //     },
+      // });
+      console.log('Push Sent');
+    } catch (e) {
+      console.log('SW faild');
     }
+  }
 })();
 
 /**
  * プルダウンの値を設定
  */
 async function updateSources() {
-    const res = await fetch(`https://newsapi.org/v2/sources?apiKey=${NEWS_API_KEY}`);
-    const json = await res.json();
+  const res = await fetch(`https://newsapi.org/v2/sources?apiKey=${NEWS_API_KEY}`);
+  const json = await res.json();
 
-    json.sources.forEach(src => {
-        sourceSelector.insertAdjacentHTML("beforeend", `<option value="${src.id}">${src.name}</option>`);
-    });
+  json.sources.forEach((src) => {
+    sourceSelector.insertAdjacentHTML(
+      'beforeend',
+      `<option value="${src.id}">${src.name}</option>`,
+    );
+  });
 }
 
 /**
  * ニュースの内容を表示
- * @param {string} source 
+ * @param {string} source
  */
 async function updateNews(source = defaultSource) {
-    const res = await fetch(`https://newsapi.org/v2/everything?q=${source}&apiKey=${NEWS_API_KEY}`);
-    const json = await res.json();
+  const res = await fetch(`https://newsapi.org/v2/everything?q=${source}&apiKey=${NEWS_API_KEY}`);
+  const json = await res.json();
 
-    main.innerHTML = "";
-    json.articles.forEach(article => {
-        main.insertAdjacentHTML("beforeend", `
+  main.innerHTML = '';
+  json.articles.forEach((article) => {
+    main.insertAdjacentHTML(
+      'beforeend',
+      `
             <div class="article">
                 <a href="${article.url}">
                     <h2>${article.title}</h2>
@@ -70,25 +86,24 @@ async function updateNews(source = defaultSource) {
                     <p>${article.description}</p>
                 </a>
             </div>
-        `);
-    });
+        `,
+    );
+  });
 }
 
 /**
  * @see https://github.com/web-push-libs/web-push#using-vapid-key-for-applicationserverkey
- * @param {string} base64String 
+ * @param {string} base64String
  */
 function urlBase64ToUint8Array(base64String) {
-    const padding = '='.repeat((4 - base64String.length % 4) % 4);
-    const base64 = (base64String + padding)
-        .replace(/-/g, '+')
-        .replace(/_/g, '/');
+  const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding).replace(/-/g, '+').replace(/_/g, '/');
 
-    const rawData = window.atob(base64);
-    const outputArray = new Uint8Array(rawData.length);
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
 
-    for (let i = 0; i < rawData.length; ++i) {
-        outputArray[i] = rawData.charCodeAt(i);
-    }
-    return outputArray;
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
 }
